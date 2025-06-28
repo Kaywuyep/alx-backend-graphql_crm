91afe909990eb7f5ecd3357422772e25c2863423
Catherine Wuyep <wuyepkate@gmail.com>
2025-06-28 16:35:37 +0100

    Set Up GraphQL Endpoint

--- alx-backend-graphql_crm/settings.py
+++ alx-backend-graphql_crm/settings.py
@@ -52,10 +52,10 @@ MIDDLEWARE = [
 ]
 
 GRAPHENE = {
-    "SCHEMA": "alx_backend_graphql_crm.schema.schema"  # path to the schema object
+    "SCHEMA": "alx-backend-graphql_crm.schema.schema"  # path to the schema object
 }
 
-ROOT_URLCONF = 'alx_backend_graphql_crm.urls'
+ROOT_URLCONF = 'alx-backend-graphql_crm.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'alx_backend_graphql_crm.wsgi.application'
+WSGI_APPLICATION = 'alx-backend-graphql_crm.wsgi.application'
 
 
 # Database
